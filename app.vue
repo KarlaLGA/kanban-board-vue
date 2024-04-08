a8a6c95c17424b8e6f1cbea4cf9f405ca363f865
karlalga <karla@karlalopez3d.com>
2024-04-08 12:16:01 -0700

    TrelloBoardTask component

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="p-10 h-[100vh] overflow-auto bg-cyan-600">
+    <h1 class="text-4xl text-white mb-10">Trello Board</h1>
     <TrelloBoard />
   </div>
 </template>
--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
-        <div v-for="column in columns" :key="column.id">
-            <h2>{{ column.title }}</h2>
-            <p v-for="task in column.tasks" :key="task.id">{{ task.title }}</p>
+    <div class="flex gap-4 overflow-x-auto items-start">
+        <div v-for="column in columns" :key="column.id" class="column bg-gray-200 p-5 rounded min-w-[250px]">
+            <header class="font-bold mb-4">{{ column.title }}</header>
+            <TrelloBoardTask v-for="task in column.tasks" :task="task" :key="task.id" />
+            <footer>
+              <button class="text-gray-500">+ add a card</button>
+            </footer>
         </div>
     </div>
 </template>
