e7b430a056d34ece89fc8e2780fc30db66eca308
karlalga <karla@karlalopez3d.com>
2024-04-10 08:56:10 -0700

    Styling fixes

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-10 h-[100vh] overflow-auto bg-cyan-600">
+  <div class="p-10 h-[100vh] overflow-auto bg-cyan-600 flex flex-col">
     <h1 class="text-4xl text-white mb-10">Trello Board</h1>
     <TrelloBoard />
   </div>
